11d5749fc3ebfd3379c62af2d65f6427f1f041a4
brunojoao <bruno.garcia@essentia.com.br>
2023-11-14 18:03:09 -0300

    telescope hubble

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -1,53 +1,79 @@
 return {
     "nvim-telescope/telescope.nvim",
-    dependencies = {
-        {
-            "nvim-telescope/telescope-fzf-native.nvim",
-            enabled = vim.fn.executable "make" == 1,
-            build = "make"
-        }
-    },
-    defaults = {
-        file_ignore_patterns = {
-            "node_modules", "vendor", ".git", ".idea", ".vscode", ".pyc", ".pyo"
-        },
-        vimgrep_arguments = vimgrep_arguments
-    },
-    pickers = {
-        find_files = {
-            hidden = true, -- will still show the inside of `.git/` as it's not `.gitignore`d.
-            find_command = {"rg", "--files", "--hidden", "--glob", "!**/.git/*"}
-        }
-    },
-    cmd = "Telescope",
-    opts = function()
-        local actions = require "telescope.actions"
-        local get_icon = require("astronvim.utils").get_icon
-        return {
-            defaults = {
-                git_worktrees = vim.g.git_worktrees,
-                prompt_prefix = get_icon("Selected", 1),
-                selection_caret = get_icon("Selected", 1),
-                path_display = {"truncate"},
-                sorting_strategy = "ascending",
-                layout_config = {
-                    horizontal = {prompt_position = "top", preview_width = 0.55},
-                    vertical = {mirror = false},
-                    width = 0.87,
-                    height = 0.80,
-                    preview_cutoff = 120
-                },
-                mappings = {
-                    i = {
-                        ["<C-n>"] = actions.cycle_history_next,
-                        ["<C-p>"] = actions.cycle_history_prev,
-                        ["<C-j>"] = actions.move_selection_next,
-                        ["<C-k>"] = actions.move_selection_previous
+    lazy = false,
+    opt = true,
+    config = function()
+        opts = function()
+            local actions = require "telescope.actions"
+            local get_icon = require("astronvim.utils").get_icon
+            return {
+                defaults = {
+                    file_ignore_patterns = {
+                        "node_modules", "build", "dist", "yarn.lock", ".git",
+                        "vendor"
                     },
-                    n = {q = actions.close}
+                    -- configure to use ripgrep
+                    vimgrep_arguments = {
+                        "rg", "--follow", -- Follow symbolic links
+                        "--hidden", -- Search for hidden files
+                        "--no-heading", -- Don't group matches by each file
+                        "--with-filename", -- Print the file path with the matched lines
+                        "--line-number", -- Show line numbers
+                        "--column", -- Show column numbers
+                        "--smart-case", -- Smart case search
+                        -- Exclude some patterns from search
+                        "--glob=!**/.git/*", "--glob=!**/.idea/*",
+                        "--glob=!**/.vscode/*", "--glob=!**/build/*",
+                        "--glob=!**/dist/*", "--glob=!**/yarn.lock",
+                        "--glob=!**/package-lock.json", "--glob=!**/vendor/*",
+                        "--glob=!**/node_modules/*",
+                        "--glob=!**/composer-lock.json"
+                    },
+
+                    pickers = {
+                        find_files = {
+                            hidden = true,
+                            -- needed to exclude some files & dirs from general search
+                            -- when not included or specified in .gitignore
+                            find_command = {
+                                "rg", "--files", "--hidden",
+                                "--glob=!**/.git/*", "--glob=!**/.idea/*",
+                                "--glob=!**/.vscode/*", "--glob=!**/build/*",
+                                "--glob=!**/dist/*", "--glob=!**/yarn.lock",
+                                "--glob=!**/package-lock.json",
+                                "--glob=!**/composer-lock.json",
+                                "--glob=!**/vendor/*",
+                                "--glob=!**/node_modules/*"
+                            }
+                        }
+                    },
+                    git_worktrees = vim.g.git_worktrees,
+                    prompt_prefix = string.format("%s ", get_icon "Search"),
+                    selection_caret = string.format("%s ", get_icon "Selected"),
+                    path_display = {"truncate"},
+                    sorting_strategy = "ascending",
+                    layout_config = {
+                        horizontal = {
+                            prompt_position = "top",
+                            preview_width = 0.55
+                        },
+                        vertical = {mirror = false},
+                        width = 0.87,
+                        height = 0.80,
+                        preview_cutoff = 120
+                    },
+
+                    mappings = {
+                        i = {
+                            ["<C-n>"] = actions.cycle_history_next,
+                            ["<C-p>"] = actions.cycle_history_prev,
+                            ["<C-j>"] = actions.move_selection_next,
+                            ["<C-k>"] = actions.move_selection_previous
+                        },
+                        n = {["q"] = actions.close}
+                    }
                 }
             }
-        }
-    end,
-    config = require "plugins.configs.telescope"
+        end
+    end
 }
