d0fb0fb210c1c6c1868b8276615389d6d4422840
brunojoao <brunokataklysm@gmail.com>
2023-11-15 20:44:46 -0300

    many snippets and menu_items

---
+++ mappings.lua
@@ -0,0 +1,8 @@
+return {
+  n = {
+    ["<leader>fS"] = { "<cmd>Spectre<cr>", desc = "Spectre Patronum!" },
+    ["<leader>gB"] = { "<cmd>GitBlameToggle<cr>", desc = "Blame Toggle" },
+    ["<leader>a"] = { desc = "Artificial intelligence" },
+    ["<leader>ac"] = { "<cmd>Codeium Auth<cr>", desc = "Codeium Auth" },
+  },
+}
