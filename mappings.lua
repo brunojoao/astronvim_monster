fab2477fb3b41699d56cbd175697aca907c285ec
brunojoao <bruno.garcia@essentia.com.br>
2023-11-16 11:04:03 -0300

    New mappings git blame

--- mappings.lua
+++ mappings.lua
@@ -1,8 +1,18 @@
 return {
-  n = {
-    ["<leader>fS"] = { "<cmd>Spectre<cr>", desc = "Spectre Patronum!" },
-    ["<leader>gB"] = { "<cmd>GitBlameToggle<cr>", desc = "Blame Toggle" },
-    ["<leader>a"] = { desc = "Artificial intelligence" },
-    ["<leader>ac"] = { "<cmd>Codeium Auth<cr>", desc = "Codeium Auth" },
-  },
+    n = {
+        ["<leader>fS"] = {"<cmd>Spectre<cr>", desc = "Spectre Patronum!"},
+        ["<leader>gB"] = {"<cmd>GitBlameToggle<cr>", desc = "Blame Toggle"},
+        ["<leader>a"] = {desc = "Artificial intelligence"},
+        ["<leader>ac"] = {"<cmd>Codeium Auth<cr>", desc = "Codeium Auth"},
+        ["<leader>G"] = {desc = "GitBlame"},
+        ["<leader>Gt"] = {"<cmd>GitBlameToggle<cr>", desc = "Blame Toggle"},
+        ["<leader>Gc"] = {
+            "<cmd>GitBlameOpenCommitURL<cr>",
+            desc = "Blame open commit"
+        },
+        ["<leader>Gf"] = {
+            "<cmd>GitBlameOpenFileURL<cr>",
+            desc = "Blame open file"
+        }
+    }
 }
