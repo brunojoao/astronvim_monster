aadf64d123e17761a52cd7e5398f03c48fc4168c
brunojoao <bruno.garcia@essentia.com.br>
2023-11-23 15:43:58 -0300

    codeium

--- plugins/codeium.lua
+++ plugins/codeium.lua
@@ -1,11 +1,28 @@
 return {
-  {
-    "Exafunction/codeium.nvim",
-    lazy = false,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-    },
-    config = function() require("codeium").setup {} end,
-  },
+    {
+        "Exafunction/codeium.nvim",
+        lazy = false,
+        dependencies = {"nvim-lua/plenary.nvim", "hrsh7th/nvim-cmp"},
+        config = function()
+            local lspkind = require("lspkind")
+            local cmp = require("cmp")
+            require("codeium").setup {
+                cmp.setup({
+                    -- ...
+                    sources = {
+                        -- ...
+                        {name = "codeium"}
+                    }
+                }),
+                formatting = {
+                    format = lspkind.cmp_format({
+                        mode = "symbol",
+                        maxwidth = 50,
+                        ellipsis_char = '...',
+                        symbol_map = {Codeium = ""}
+                    })
+                }
+            }
+        end
+    }
 }
