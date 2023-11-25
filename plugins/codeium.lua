1c867bbfc35ca95fad15a5943872a32c96164a21
brunojoao <brunokataklysm@gmail.com>
2023-11-25 01:02:05 -0300

    codeium and templates snipets fix

--- plugins/codeium.lua
+++ plugins/codeium.lua
@@ -3,26 +3,5 @@ return {
     "Exafunction/codeium.nvim",
     lazy = false,
     dependencies = { "nvim-lua/plenary.nvim", "hrsh7th/nvim-cmp" },
-    config = function()
-      local lspkind = require "lspkind"
-      local cmp = require "cmp"
-      require("codeium").setup {
-        cmp.setup {
-          -- ...
-          sources = {
-            -- ...
-            { name = "codeium" },
-          },
-        },
-        formatting = {
-          format = lspkind.cmp_format {
-            mode = "symbol",
-            maxwidth = 50,
-            ellipsis_char = "...",
-            symbol_map = { Codeium = "" },
-          },
-        },
-      }
-    end,
   },
 }
