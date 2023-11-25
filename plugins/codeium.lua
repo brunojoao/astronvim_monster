15aed5c823ed628f359cb6e212272ae945326c53
brunojoao <brunokataklysm@gmail.com>
2023-11-24 22:01:09 -0300

    surreound mini

--- plugins/codeium.lua
+++ plugins/codeium.lua
@@ -1,28 +1,28 @@
 return {
-    {
-        "Exafunction/codeium.nvim",
-        lazy = true,
-        dependencies = {"nvim-lua/plenary.nvim", "hrsh7th/nvim-cmp"},
-        config = function()
-            local lspkind = require("lspkind")
-            local cmp = require("cmp")
-            require("codeium").setup {
-                cmp.setup({
-                    -- ...
-                    sources = {
-                        -- ...
-                        {name = "codeium"}
-                    }
-                }),
-                formatting = {
-                    format = lspkind.cmp_format({
-                        mode = "symbol",
-                        maxwidth = 50,
-                        ellipsis_char = '...',
-                        symbol_map = {Codeium = ""}
-                    })
-                }
-            }
-        end
-    }
+  {
+    "Exafunction/codeium.nvim",
+    lazy = false,
+    dependencies = { "nvim-lua/plenary.nvim", "hrsh7th/nvim-cmp" },
+    config = function()
+      local lspkind = require "lspkind"
+      local cmp = require "cmp"
+      require("codeium").setup {
+        cmp.setup {
+          -- ...
+          sources = {
+            -- ...
+            { name = "codeium" },
+          },
+        },
+        formatting = {
+          format = lspkind.cmp_format {
+            mode = "symbol",
+            maxwidth = 50,
+            ellipsis_char = "...",
+            symbol_map = { Codeium = "" },
+          },
+        },
+      }
+    end,
+  },
 }
