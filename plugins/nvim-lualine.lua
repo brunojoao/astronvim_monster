a53baf3736390997def67f9edc4a2e76b750baa8
brunojoao <brunokataklysm@gmail.com>
2023-12-03 00:53:15 -0300

    better lualine config

--- plugins/nvim-lualine.lua
+++ plugins/nvim-lualine.lua
@@ -5,7 +5,7 @@ return {
     require("lualine").setup {
       options = {
         icons_enabled = true,
-        theme = "auto",
+        theme = "horizon",
         component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
         disabled_filetypes = {
@@ -24,14 +24,14 @@ return {
       sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { "filename" },
+        lualine_c = { "searchcount", "selectioncount" },
         lualine_x = { "encoding", "fileformat", "filetype" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
       },
       inactive_sections = {
         lualine_a = {},
-        lualine_b = {},
+        lualine_b = { "branch" },
         lualine_c = { "filename" },
         lualine_x = { "location" },
         lualine_y = {},
