422bdecfa50b30b1282674a5053ef5481a67550a
brunojoao <bruno.garcia@essentia.com.br>
2023-12-12 17:21:55 -0300

    lazy refatoring

--- plugins/refactoring.lua
+++ plugins/refactoring.lua
@@ -1,5 +1,6 @@
 return {
     "ThePrimeagen/refactoring.nvim",
+    lazy = false,
     dependencies = {"nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter"},
     config = function() require("refactoring").setup() end
 }
