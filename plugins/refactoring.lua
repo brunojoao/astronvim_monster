ada638a90e8ad6258b04cd7bc9f16babf0c03fa9
brunojoao <bruno.garcia@essentia.com.br>
2023-12-12 17:21:23 -0300

    fix refactoring ,

--- plugins/refactoring.lua
+++ plugins/refactoring.lua
@@ -1,10 +1,5 @@
 return {
     "ThePrimeagen/refactoring.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("refactoring").setup()
-    end,
-  },
+    dependencies = {"nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter"},
+    config = function() require("refactoring").setup() end
+}
