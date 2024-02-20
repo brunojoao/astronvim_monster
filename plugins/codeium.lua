b8834da50b120519dc2800ddce3a106f4c26cda7
brunojoao <bruno.garcia@essentia.com.br>
2024-02-20 16:57:23 -0300

    fix codeium

--- plugins/codeium.lua
+++ plugins/codeium.lua
@@ -1,20 +1,18 @@
 return {
-    {
-        "Exafunction/codeium.nvim",
-        lazy = false,
-        dependencies = {"nvim-lua/plenary.nvim", "hrsh7th/nvim-cmp"},
-        config = function()
-            local lspkind = require "lspkind"
-            require("codeium").setup {
-                formatting = {
-                    format = lspkind.cmp_format {
-                        mode = "symbol",
-                        maxwidth = 150,
-                        ellipsis_char = "...",
-                        symbol_map = {Codeium = ""}
-                    }
+    "Exafunction/codeium.nvim",
+    lazy = false,
+    dependencies = {"nvim-lua/plenary.nvim", "hrsh7th/nvim-cmp"},
+    config = function()
+        local lspkind = require "lspkind"
+        require("codeium").setup {
+            formatting = {
+                format = lspkind.cmp_format {
+                    mode = "symbol",
+                    maxwidth = 150,
+                    ellipsis_char = "...",
+                    symbol_map = {Codeium = ""}
                 }
             }
-        end
-    }
+        }
+    end
 }
