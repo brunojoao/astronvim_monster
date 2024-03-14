5b2c14eceedcfc336deff26f858d92680597ba7c
brunojoao <bruno.garcia@essentia.com.br>
2024-03-14 14:05:17 -0300

    lsp improve

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
 return {
     lsp = {
         formatting = {
-            format_on_save = true -- enable or disable automatic formatting on save
+            format_on_save = true, -- enable or disable automatic formatting on save
+            timeout_ms = 50000
         }
     },
     plugins = {
