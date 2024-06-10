ca89e9f1c463921ced23ce8d3b2b61c0c9e1c87c
brunojoao <bruno.garcia@essentia.com.br>
2024-06-10 11:47:55 -0300

    laravel and blade add

--- plugins/laravel.lua
+++ plugins/laravel.lua
@@ -1,5 +1,6 @@
 return {
     "adalessa/laravel.nvim",
+    lazy = false,
     dependencies = {
         "nvim-telescope/telescope.nvim", "tpope/vim-dotenv",
         "MunifTanjim/nui.nvim", "nvimtools/none-ls.nvim"
